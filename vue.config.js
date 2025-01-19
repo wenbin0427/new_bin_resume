153e0882d993e12c7b1996a404f55c364cffe2dd
wenbin0427 <a30082424@gmail.com>
2025-01-19 17:34:26 +0800

    up_config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/new_bin_resume/' // 把 <repository> 換成你的 GitHub 專案名稱
+    : '/'
 })
