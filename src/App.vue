2e294b22ae9f6d4c2aba326dfc6f8472eb34630f
wenbin0427 <a30082424@gmail.com>
2025-03-24 02:44:54 +0800

    Create gh-pages branch

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+import AOS from 'aos';
+
+export default{
+  name:'App',
+  components:{
+    NavBar,
+  },
+
+};
+AOS.init();
+
+</script>
